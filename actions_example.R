ab281908ac25d316480e468c01e68e2e9d883d6f
Dmitry O. Gorodnichy <dmitry.gorodnichy@gmail.com>
2021-09-03 11:45:13 -0400

    covid_records<-data.table(ID,title,org)

--- actions_example.R
+++ actions_example.R
@@ -1,8 +1,9 @@
 system("sudo apt-get install libcurl4-openssl-dev") #the github VM needs this
 
-install.packages("ckanr") #you need to install the packages everytime since it is a fresh container
-
-library("ckanr")
+install.packages("ckanr") #you need to install the packages every time since it is a fresh container
+install.packages(data.table)
+library(ckanr)
+library(data.table)
 
 ckanr_setup(url="https://open.canada.ca/data")
 
@@ -12,8 +13,8 @@ coviddata<-package_search(q="COVID", rows="10000", as="table")
 ID<-coviddata$results$id
 title<-coviddata$results$title_translated$en
 org<-coviddata$results$organization$title
-covid_records<-data.frame(ID,title,org)
+covid_records<-data.table(ID,title,org)
 
 setwd("/home/runner/work/lunch_and_learn_opengov/lunch_and_learn_opengov")
 
-write.table(covid_records,file="example_table.csv",append=F,row.names = F,col.names = T, sep = ",")
+fwrite(covid_records,file="example_table.csv")
